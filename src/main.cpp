6be9606db99a673d7d22e5ac3f5a9c01477ab8e2
Anthony Libardi <Anthony Libardi@localhost>
2017-02-06 14:40:03 -0500

    [+] Cmake generates properly now.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
+using namespace std;
+
 #include "SDL.h"
+#undef main
+
 #include "Box2D/Box2D.h"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
+	cout << "Hello SDL!\n";
+	cin.get();
+
 	return 0;
 }
