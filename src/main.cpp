62664cde765da4414815027b00914a2ecaead1a9
Anthony Libardi <libardi42@gmail.com>
2017-02-06 15:19:18 -0500

    [+] CMake copies the DLL, and the window opens. Also added a waitAndExit(int) function to call cin.get() and then exit.

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,30 @@ using namespace std;
 
 #include "Box2D/Box2D.h"
 
+void waitAndExit(int exitCode)
+{
+	cin.get();
+	exit(exitCode);
+}
+
 int main(int argc, char *argv[])
 {
-	cout << "Hello SDL!\n";
-	cin.get();
+	SDL_Window *window = nullptr;
+	SDL_Surface *screen = nullptr;
 
-	return 0;
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		cout << "Error initting SDL Video: " << SDL_GetError() << endl;
+		waitAndExit(-1);
+	}
+
+	window = SDL_CreateWindow("Hello SDL!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+
+	if (window == nullptr)
+	{
+		cout << "Window could not be created: " << SDL_GetError() << endl;
+		waitAndExit(-1);
+	}
+
+	waitAndExit(0);
 }
