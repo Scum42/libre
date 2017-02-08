a2494b78f0424d897eb63104ce733dd34b0486f8
Anthony Libardi <Anthony Libardi@localhost>
2017-02-08 12:14:36 -0500

    [+] Added SDL_image, and a drawn image. Stub of Game class and tools folder.

--- src/main.cpp
+++ src/main.cpp
@@ -2,34 +2,46 @@
 using namespace std;
 
 #include "SDL.h"
+#include "SDL_image.h"
 #undef main
 
 #include "Box2D/Box2D.h"
-
-void waitAndExit(int exitCode)
-{
-	cin.get();
-	exit(exitCode);
-}
 
 int main(int argc, char *argv[])
 {
 	SDL_Window *window = nullptr;
 	SDL_Surface *screen = nullptr;
+	SDL_Renderer *renderer = nullptr;
 
-	if (SDL_Init(SDL_INIT_VIDEO) != 0)
-	{
-		cout << "Error initting SDL Video: " << SDL_GetError() << endl;
-		waitAndExit(-1);
-	}
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) return 1;
 
 	window = SDL_CreateWindow("Hello SDL!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+	if (window == nullptr) return 1;
 
-	if (window == nullptr)
+	renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 114, 144, 154, 255);
+
+	SDL_Surface *surf = IMG_Load("../res/stones.png");
+	SDL_Texture *tex = SDL_CreateTextureFromSurface(renderer, surf);
+	SDL_FreeSurface(surf);
+
+	while (true)
 	{
-		cout << "Window could not be created: " << SDL_GetError() << endl;
-		waitAndExit(-1);
-	}
+		SDL_Event e;
+		while (SDL_PollEvent(&e))
+		{
+			//;
+		}
 
-	waitAndExit(0);
+		SDL_Rect destRect;
+		destRect.x = 0;
+		destRect.y = 0;
+		destRect.w = 500;
+		destRect.h = 325;
+
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, tex, nullptr, nullptr);
+		SDL_RenderPresent(renderer);
+	}
 }
