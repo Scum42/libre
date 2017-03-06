8658424920d02b4dd0f6b9b9d8d09304fe44ec03
Anthony Libardi <Anthony Libardi@localhost>
2017-03-06 00:39:23 -0500

    [+-] Added Component, GameObject, and Transform stubs, and removed the commented code in main.

---
+++ libre/Transform.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "LibreMath.h"
+
+namespace libre
+{
+    struct Transform
+    {
+        Vector2f position;
+        Vector2f scale;
+        float rotation;
+    };
+}
--- libreDemo/main.cpp
+++ libreDemo/main.cpp
@@ -1,54 +1,6 @@
-/*
-#include <iostream>
-using namespace std;
-
-#include "SDL.h"
-#include "SDL_image.h"
-#include "Box2D/Box2D.h"
-#undef main
-*/
-
 #include "MyGame.h"
 
 int main(int argc, char *argv[])
 {
-    /*
-    SDL_Window *window = nullptr;
-    SDL_Surface *screen = nullptr;
-    SDL_Renderer *renderer = nullptr;
-
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) return 1;
-
-    window = SDL_CreateWindow("Hello SDL!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1600, 900, SDL_WINDOW_FULLSCREEN_DESKTOP);
-    if (window == nullptr) return 1;
-
-    renderer = SDL_CreateRenderer(window, -1, 0);
-
-    SDL_SetRenderDrawColor(renderer, 114, 144, 154, 255);
-
-    SDL_Surface *surf = IMG_Load("../res/stones.png");
-    SDL_Texture *tex = SDL_CreateTextureFromSurface(renderer, surf);
-    SDL_FreeSurface(surf);
-
-    while (true)
-    {
-        SDL_Event e;
-        while (SDL_PollEvent(&e))
-        {
-            //;
-        }
-
-        SDL_Rect destRect;
-        destRect.x = 0;
-        destRect.y = 0;
-        destRect.w = 500;
-        destRect.h = 325;
-
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, tex, nullptr, nullptr);
-        SDL_RenderPresent(renderer);
-    }
-    */
-
     return MyGame::GetInstance()->Start();
 }
